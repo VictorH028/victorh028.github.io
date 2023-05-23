6d746dee3292599f7015ca0ea5a5929bf27d227b
VictorH028 <Demonhunternauta@gmail.com>
2023-05-22 21:27:25 -0400

    0.0.0.1

---
+++ _pages/about.md
@@ -0,0 +1,9 @@
+---
+layout: splash
+title: "Información del autor"
+permalink: /about/
+date: 2020-01-29
+---
+
+
+Mi nombre es Victor Hugo Negret Torres ( Demon Hunter ).
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Como intalar localtunnel",
+        "excerpt":"En esta oportunidad vamos a aprender como instalar localtunel en nuestra terminal sin errores Instalacion Paso 1: actualizar paquetes Ejecute el siguiente comando apt update &amp;&amp; apt upgrade -y Paso 2: Instalar termux-api Ahora debe asegurarse de que termux tenga acceso a las funciones de hardware de Android y Chrome....","categories": ["soluciones"],
+        "tags": ["error","termux"],
+        "url": "/install-localtunel/",
+        "teaser": "/assets/images/localtunnel/icon-localtunnel.png"
+      },{
+        "title": "Alpine en termux",
+        "excerpt":"Para tener coriendo Alpine en nuestro dispositivo vamos a nesecitar los siquiente con la finalidad de facilitar el uso de Termux y mantenerlo lo más similar posible a las distribuciones Linux @Ivam3 a realizado la adaptación al gestor de paquetes Advance Package Tool (APT) de las herramientas y/o frameworks que...","categories": ["termux"],
+        "tags": ["termux","intalacion"],
+        "url": "/Alpine/",
+        "teaser": null
+      }]
