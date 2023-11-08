686f11c7e565606d39678540bdbb8d8c4ac43b13
VictorH028 <Demonhunternauta@gmail.com>
2023-11-08 09:05:40 -0500

    i-Haklab.a

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,48 +2,40 @@ var store = [{
         "title": "Como intalar localtunnel",
         "excerpt":"En esta oportunidad vamos a aprender como instalar localtunel en nuestra terminal sin errores Instalacion Paso 1: actualizar paquetes Ejecute el siguiente comando apt update &amp;&amp; apt upgrade -y Paso 2: Instalar termux-api Ahora debe asegurarse de que termux tenga acceso a las funciones de hardware de Android y Chrome....","categories": ["termux"],
         "tags": ["error","termux"],
-        "url": "/install-localtunel/",
-        "teaser": "/assets/images/localtunnel/icon-localtunnel.png"
-      },{
+        "url": "http://localhost:4000/install-localtunel/",
+        "teaser":"http://localhost:4000/assets/images/localtunnel/icon-localtunnel.png"},{
         "title": "Alternativas para `ngrok`",
         "excerpt":"Ngrok es una aplicación desarrollada por Alan Shreeve , permite a los desarrolladores exponer sus servidores de desarrollo locales a Internet. Básicamente, crea un túnel a su servidor de desarrollo local y genera dos subdominios aleatorios en ngrok.com, uno http y otro con https pero en ocasiones tenemos problemas con...","categories": ["alternativa"],
         "tags": ["termux","tips"],
-        "url": "/ngrok_alternativas/",
-        "teaser": "/assets/images/ngrok/ngrok-blue.png"
-      },{
+        "url": "http://localhost:4000/ngrok_alternativas/",
+        "teaser":"http://localhost:4000/assets/images/ngrok/ngrok-blue.png"},{
         "title": "Notas de git",
         "excerpt":"Para evitar tener que autenticarte cada vez que subes un proyecto a Git desde tu terminal, puedes usar un token de acceso personal de Git. Un token de acceso personal es una cadena de caracteres que funciona como una contraseña temporal que solo se te otorga una vez. Con este...","categories": ["soluciones","notas"],
         "tags": ["termux","tips"],
-        "url": "/GIT/",
-        "teaser": "/assets/images/GIT/git-blak.png"
-      },{
+        "url": "http://localhost:4000/GIT/",
+        "teaser":"http://localhost:4000/assets/images/GIT/git-blak.png"},{
         "title": "Alpine en termux",
         "excerpt":"Para tener coriendo Alpine en nuestro dispositivo vamos a nesecitar los siquiente con la finalidad de facilitar el uso de Termux y mantenerlo lo más similar posible a las distribuciones Linux @Ivam3 a realizado la adaptación al gestor de paquetes Advance Package Tool (APT) de las herramientas y/o frameworks que...","categories": ["termux","root"],
         "tags": ["termux","intalacion"],
-        "url": "/Alpine/",
-        "teaser": null
-      },{
+        "url": "http://localhost:4000/Alpine/",
+        "teaser":null},{
         "title": "i-Haklab",
-        "excerpt":"Laboratorios Hacking i-Haklab es un laboratorio de hacking para Termux que que nos ofrece de manera ordenada y secuencial un conjunto herramientas de código abierto para pentesting, que nos proporciona mas de 100 herramientas/marcos que no estan disponible en termux. Imagen Intalacion Su intalacion no es como estamos acostumbrados hacer...","categories": ["Laboratorio"],
+        "excerpt":"Menu i-Haklab es un laboratorio de hacking para Termux que que nos ofrece de manera ordenada y secuencial un conjunto herramientas de código abierto para pentesting, que nos proporciona mas de 100 herramientas/marcos que no estan disponible en termux. Imagen Intalacion Su intalacion no es como estamos acostumbrados hacer avitual...","categories": ["Laboratorio"],
         "tags": ["Termux"],
-        "url": "/i_Haklab/",
-        "teaser": "/assets/images/"
-      },{
+        "url": "http://localhost:4000/i_Haklab/",
+        "teaser":"http://localhost:4000/assets/images/"},{
         "title": "Criptografía_RSA",
         "excerpt":"En esta oportinidad le enseñare como cifrará un archivo y la creacion de tu llave publia y tu llave privada en unos simple pasos. Antes asegurarse de que tienes OpenSSL instalado. Si no dispones del mismo, se puede instalar esta herramienta con el siguiente comando: apt update apt install openssl...","categories": [null,""],
         "tags": ["RSA"],
-        "url": "/RSA/",
-        "teaser": null
-      },{
+        "url": "http://localhost:4000/RSA/",
+        "teaser":null},{
         "title": "pendiente",
         "excerpt":"    Please enable JavaScript to view the comments powered by Disqus.  ","categories": ["termux"],
         "tags": ["alpine"],
-        "url": "/aline-get_file/",
-        "teaser": "/assets/images/"
-      },{
+        "url": "http://localhost:4000/aline-get_file/",
+        "teaser":"http://localhost:4000/assets/images/"},{
         "title": "desktop",
         "excerpt":"Para mostrar tu aplicaciones en el menu de Linux, puedes crear un archivo .desktop. Estos archivos se utilizan para definir las propiedades de una aplicación y cómo se mostrará en el menú de aplicaciones. A continuación, te proporciono un ejemplo de cómo crear un archivo .desktop.: Abre un editor de...","categories": ["notas"],
         "tags": ["Linux","tips"],
-        "url": "/desktop-linux/",
-        "teaser": "/assets/images/"
-      }]
+        "url": "http://localhost:4000/desktop-linux/",
+        "teaser":"http://localhost:4000/assets/images/"}]
