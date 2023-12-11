f3792e64727e261131d86fb26cf1ccc78520364c
VictorH028 <demonhunternauta@gmail.com>
2023-12-10 20:27:18 -0500

    .

---
+++ _site/README.md
@@ -0,0 +1,4 @@
+# victorh028.github.io
+Actualmente, estoy sumergiéndome en el vasto océano de la ciberseguridad, aprendiendo los conceptos básicos de la criptografía, la protección de redes, la detección de amenazas y más. 
+Estoy emocionado por explorar este campo en constante evolución y contribuir de manera significativa en la protección de la información crucial.
+Pensando siempre en esas personas que le hemociona ese mundo y no cuentan con una PC, para eso esta este espacio.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,48 +2,48 @@ var store = [{
         "title": "Como intalar localtunnel",
         "excerpt":"En esta oportunidad vamos a aprender como instalar localtunel en nuestra terminal sin errores Instalacion Paso 1: actualizar paquetes Ejecute el siguiente comando apt update &amp;&amp; apt upgrade -y Paso 2: Instalar termux-api Ahora debe asegurarse de que termux tenga acceso a las funciones de hardware de Android y Chrome....","categories": ["termux"],
         "tags": ["error","termux"],
-        "url": "/install-localtunel/",
+        "url": "/termux/2023/04/17/install-localtunel.html",
         "teaser": "/assets/images/localtunnel/icon-localtunnel.png"
       },{
         "title": "Alternativas para `ngrok`",
         "excerpt":"Ngrok es una aplicación desarrollada por Alan Shreeve , permite a los desarrolladores exponer sus servidores de desarrollo locales a Internet. Básicamente, crea un túnel a su servidor de desarrollo local y genera dos subdominios aleatorios en ngrok.com, uno http y otro con https pero en ocasiones tenemos problemas con...","categories": ["alternativa"],
         "tags": ["termux","tips"],
-        "url": "/ngrok_alternativas/",
+        "url": "/alternativa/2023/04/18/ngrok_alternativas.html",
         "teaser": "/assets/images/ngrok/ngrok-blue.png"
       },{
         "title": "Notas de git",
         "excerpt":"Para evitar tener que autenticarte cada vez que subes un proyecto a Git desde tu terminal, puedes usar un token de acceso personal de Git. Un token de acceso personal es una cadena de caracteres que funciona como una contraseña temporal que solo se te otorga una vez. Con este...","categories": ["soluciones","notas"],
         "tags": ["termux","tips"],
-        "url": "/GIT/",
+        "url": "/soluciones/notas/2023/04/20/GIT.html",
         "teaser": "/assets/images/GIT/git-blak.png"
       },{
         "title": "Alpine en termux",
         "excerpt":"Para tener coriendo Alpine en nuestro dispositivo vamos a nesecitar los siquiente con la finalidad de facilitar el uso de Termux y mantenerlo lo más similar posible a las distribuciones Linux @Ivam3 a realizado la adaptación al gestor de paquetes Advance Package Tool (APT) de las herramientas y/o frameworks que...","categories": ["termux","root"],
         "tags": ["termux","intalacion"],
-        "url": "/Alpine/",
+        "url": "/termux/root/2023/04/21/Alpine.html",
         "teaser": null
       },{
         "title": "i-Haklab",
         "excerpt":"i-Haklab es un laboratorio de hacking para Termux que que nos ofrece de manera ordenada y secuencial un conjunto herramientas de código abierto para pentesting, que nos proporciona mas de 100 herramientas/marcos que no estan disponible en termux. 📥 Intalacion Su intalacion no es como estamos acostumbrados hacer avitual mente...","categories": ["Laboratorio"],
         "tags": ["Termux"],
-        "url": "/i-Haklab/",
+        "url": "/laboratorio/2023/04/29/i-Haklab.html",
         "teaser": "/assets/images/i-haklab/logo_de_inivio_de_i-haklab.jpeg"
       },{
         "title": "RSA",
         "excerpt":"En esta oportinidad le enseñare como cifrará un archivo y la creacion de tu llave publia y tu llave privada en unos simple pasos. Antes asegurarse de que tienes OpenSSL instalado. Si no dispones del mismo, se puede instalar esta herramienta con el siguiente comando: apt update apt install openssl...","categories": [null,""],
         "tags": ["RSA"],
-        "url": "/RSA/",
+        "url": "/2023/05/25/RSA.html",
         "teaser": null
       },{
         "title": "Resaltado de sintaxis en C++",
         "excerpt":"Esto es un pequeño ejemplo de como crear un resaltado de sintaxis en C++ #include &lt;iostream &gt; #include &lt;string&gt; enum class Color { Default, Black, Red, Green, Yellow, Blue, Magenta, Cyan, White }; std::string setColor(Color color); void syntax_highlight(const std::string &amp;code); int main() { std::string codigo = R\"( #include &lt;iostream&gt; int...","categories": ["Programacion"],
         "tags": ["termux","linux"],
-        "url": "/C++_sintaxis/",
+        "url": "/programacion/2023/07/05/C++_sintaxis.html",
         "teaser": "/assets/images/"
       },{
         "title": "desktop",
         "excerpt":"Para mostrar tu aplicaciones en el menu de Linux, puedes crear un archivo .desktop. Estos archivos se utilizan para definir las propiedades de una aplicación y cómo se mostrará en el menú de aplicaciones. A continuación, te proporciono un ejemplo de cómo crear un archivo .desktop.: Abre un editor de...","categories": ["notas"],
         "tags": ["Linux","tips"],
-        "url": "/desktop-linux/",
+        "url": "/notas/2023/10/10/desktop-linux.html",
         "teaser": "/assets/images/"
       }]
